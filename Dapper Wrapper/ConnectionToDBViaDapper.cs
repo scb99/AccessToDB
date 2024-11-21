e0cec0ad113676a975f7dd168bf45bbaa8f5e9c3
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-21 15:08:26 -0500

    Add //

--- Dapper Wrapper/ConnectionToDBViaDapper.cs
+++ Dapper Wrapper/ConnectionToDBViaDapper.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
 
-namespace DataAbstractions.Dapper;
+namespace DataAbstractions.Dapper; //
 
 public class ConnectionToDBViaDapper(IDbConnection connection) : IDataAccessor
 {
